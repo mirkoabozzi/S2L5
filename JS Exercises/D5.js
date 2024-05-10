e2497c48389ceda6089fc602c95b56128baf46a4
Mirko Abozzi <mirko.abozzi@gmail.com>
2024-05-10 11:57:47 +0200

    esercizio 9

--- JS Exercises/D5.js
+++ JS Exercises/D5.js
@@ -104,15 +104,38 @@ console.log("-------------------------------------Esercizio 7-------------------
 
 const justTrims = [];
 
+for (let index = 0; index < cars.length; index++) {
+  justTrims.push(cars[index].trims);
+}
+
+console.log(justTrims);
+
 /* ESERCIZIO 8
     Cicla l'array "cars" e costruisci un if/else statament per mostrare due diversi messaggi in console. Se la prima lettera della proprietà
     "color" ha valore "b", mostra in console "Fizz". Altrimenti, mostra in console "Buzz".
 */
+console.log("-------------------------------------Esercizio 8-------------------------------------");
+
+for (let index = 0; index < cars.length; index++) {
+  if (cars[index].color.charAt(0) === "b") {
+    console.log("Fritz");
+  } else {
+    console.log("Buzz");
+  }
+}
 
 /* ESERCIZIO 9
     Utilizza un ciclo while per stampare in console i valori del seguente array numerico fino al raggiungimento del numero 32.
 */
+
+console.log("-------------------------------------Esercizio 9-------------------------------------");
 const numericArray = [6, 90, 45, 75, 84, 98, 35, 74, 31, 2, 8, 23, 100, 32, 66, 313, 321, 105];
+
+let i = 0;
+while (i < numericArray.length && numericArray[i] !== 32) {
+  console.log(numericArray[i]);
+  i++;
+}
 
 /* ESERCIZIO 10
     Partendo dall'array fornito e utilizzando un costrutto switch, genera un nuovo array composto dalle posizioni di ogni carattere all'interno
